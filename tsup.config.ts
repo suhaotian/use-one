73fed0949ceef273238157eb7a606b544937dcb9
suhaotian <h4av.one@gmail.com>
2025-03-12 21:52:42 +1100

    Replace bunchee with tsup

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    'index.react-native': 'src/index.react-native.ts',
+    'index.react-server': 'src/index.react-server.ts',
+  },
+  outDir: './dist',
+  splitting: true,
+  format: ['esm', 'cjs'],
+  minify: true,
+  target: 'es2015',
+  dts: true,
+  external: ['react', '@react-native-async-storage/async-storage', 'server-only']
+});
